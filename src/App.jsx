a7e236c6e7cae8a35360ee31bc049ac1a04d5625
syeonnn <qkrtj418@gmail.com>
2024-07-31 11:10:53 +0900

    feat: 현재 위치 불러오기 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import './App.css'
+// import.meta.env.VITE_KAKAO_MAP_API_KEY
 import { useEffect, useState } from 'react';
-import './utils/getLocation';
 
-function App() {
+function Map() {
+  const [locPosition , setLocPosition] = useState();
 
   useEffect(() => {
     const mapScript = document.createElement('script');
 
     mapScript.async = true;
-    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_MAP_API_KEY}`;
+    mapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${import.meta.env.VITE_KAKAO_MAP_API_KEY}&autoload=false`;
+
     document.head.appendChild(mapScript);
 
     const onLoadKakaoMap = () => {
@@ -24,16 +25,71 @@ function App() {
     mapScript.addEventListener('load', onLoadKakaoMap);
   }, []);
 
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-      <div id="map" className="w-20 h-20">
-      </div>
+  useEffect(() => {
+    handleCurrLoc();
+    console.log('locPosition바뀜')
+    
+  }, [locPosition])
 
-    </>
-  )
+  const handleCurrLoc = () => {
+    if (navigator.geolocation) {
+    
+      // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+      navigator.geolocation.getCurrentPosition(function(position) {
+          
+          var lat = position.coords.latitude, // 위도
+              lon = position.coords.longitude; // 경도
+          
+          // var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+          //     message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+          setLocPosition(new kakao.maps.LatLng(lat, lon))
+          
+          // 마커와 인포윈도우를 표시합니다
+          displayMarker(locPosition, '<div style="padding:5px;">여기에 계신가요?!</div>');
+              
+        });
+      
+  } else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+      
+      // var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),    
+      //     message = 'geolocation을 사용할수 없어요..'
+      
+      setLocPosition(new kakao.maps.LatLng(33.450701, 126.570667));
+
+      displayMarker(locPosition, 'geolocation을 사용할수 없어요..');
+  }}
+
+  function displayMarker(locPosition, message) {
+
+    // 마커를 생성합니다
+    var marker = new kakao.maps.Marker({  
+        map: map, 
+        position: locPosition
+    }); 
+    
+    var iwContent = message; // 인포윈도우에 표시할 내용
+    var iwRemoveable = true;
+
+    // 인포윈도우를 생성합니다
+    var infowindow = new kakao.maps.InfoWindow({
+        content : iwContent,
+        removable : iwRemoveable
+    });
+    
+    // 인포윈도우를 마커위에 표시합니다 
+    infowindow.open(map, marker);
+    
+    // 지도 중심좌표를 접속위치로 변경합니다
+    map.setCenter(locPosition);      
+}    
+
+  return (
+    <div>
+      <div id="map" className="w-96 h-96"></div>
+      <button id='search_loc' onClick={handleCurrLoc}>검색</button>
+      <button id='current_loc' onClick={handleCurrLoc}>현위치</button>
+    </div>
+  );
 }
 
-export default App
+export default Map;
--- src/utils/getLocation.js
+++ src/utils/getLocation.js
@@ -1,5 +1,7 @@
-const watchID = navigator.geolocation.watchPosition((position) => {
+export const watchID = navigator.geolocation.watchPosition((position) => {
     console.log(position.coords.latitude, position.coords.longitude);
-    
+    const watchID_x = position.coords.latitude;
+    const watchID_y = position.coords.longitude;
+    return [watchID_x, watchID_y];
 });
 navigator.geolocation.clearWatch(watchID);
